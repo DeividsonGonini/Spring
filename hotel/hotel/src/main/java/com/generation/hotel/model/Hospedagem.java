7012128f755f1ca4255d6722113537d656967fbd
DeividsonGonini <deividsongonini@gmail.com>
2020-06-15 10:24:06 -0300

    Corrigida a Dependency de validação na POM e adicionado relacionamento entre Quarto e Hospedagem

---
+++ hotel/hotel/src/main/java/com/generation/hotel/model/Hospedagem.java
@@ -0,0 +1,62 @@
+package com.generation.hotel.model;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table (name = "tb_hospedagem")
+
+public class Hospedagem {
+	
+	@Column (name = "cd_hospedagem")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int codigoHospedagem;
+	
+	//faz o Relacionamento de UM para UM com a tabela Quarto
+	//Cascade ALL = se apagar um item que é linkado na tabela(nesse caso o quarto) Ele ira apagar todas as hospedagens que utilizaram aquele dado
+	//Cascade PERSIST nao deixa apagar um item (QUARTO) que esta linkado a outro (HOSPEDAGEM)
+	@OneToOne (cascade = CascadeType.PERSIST)
+	//pega o atributo abaixo dessa Tabela e associa com o Campo "cd_quarto" da tabela Quarto (tipo abaixo)
+	@JoinColumn(name = "cd_quarto" , referencedColumnName = "cd_quarto")
+	private Quarto quarto;
+		
+	private Date dataCheckin = new java.sql.Date(System.currentTimeMillis());
+
+	public int getCodigoHospedagem() {
+		return codigoHospedagem;
+	}
+
+	public void setCodigoHospedagem(int codigoHospedagem) {
+		this.codigoHospedagem = codigoHospedagem;
+	}
+
+	public Quarto getQuarto() {
+		return quarto;
+	}
+
+	public void setQuarto(Quarto quarto) {
+		this.quarto = quarto;
+	}
+
+	public Date getDataCheckin() {
+		return dataCheckin;
+	}
+
+	public void setDataCheckin(Date dataCheckin) {
+		this.dataCheckin = dataCheckin;
+	}
+
+	
+	
+}
---
+++ hotel/hotel/src/main/java/com/generation/hotel/repository/HospedagemRepository.java
@@ -0,0 +1,9 @@
+package com.generation.hotel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.hotel.model.Hospedagem;
+
+public interface HospedagemRepository extends JpaRepository<Hospedagem, Integer>{
+
+}
